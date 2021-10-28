902f5cd304224ac47b9d329d47b3528faf746b12
Dekguh <kadekteguhmahesa@gmail.com>
2021-10-28 14:34:23 +0800

    fix card own name

--- components/UI/molecules/card/CardOwnerIklan.tsx
+++ components/UI/molecules/card/CardOwnerIklan.tsx
@@ -16,7 +16,7 @@ const CardOwnerIklan : React.FC<ICardOwnerIklan> = ({ userId, onClick }) => {
     return (
         <div className='border border-gray-200 p-3 rounded-lg flex flex-row flex-nowrap items-center'>
             <div className='flex-grow flex-shrink'>
-                <h5>{dataBilling.nama}</h5>
+                <h5>{dataBilling?.nama}</h5>
                 <div className='flex flex-row flex-nowrap items-center mt-1'>
                     <span className={`${dataBilling?.isOnline ? 'bg-green-500' : 'bg-red-500'} rounded-full h-2 w-2 block mr-1`}></span>
                     <span>{dataBilling?.isOnline ? 'online' : 'offline'}</span>
