ebae539aa828de7c042aeb353d2df0a8944badbd
Dekguh <kadekteguhmahesa@gmail.com>
2022-01-10 19:13:12 +0800

    update message detail

--- components/UI/molecules/card/CardOwnerIklan.tsx
+++ components/UI/molecules/card/CardOwnerIklan.tsx
@@ -6,8 +6,6 @@ import ButtonPrimary from '../../atoms/control/ButtonPrimary'
 
 const CardOwnerIklan : React.FC<ICardOwnerIklan> = ({ userId, onClick }) => {
     const [dataBilling, setDataBilling] = useState<any>({})
-    const [dataBillingOnline, setDataBillingOnline] = useState<any>({})
-
     useEffect(() => {
         const getBilling = async () => {
             const response = await apiGetOwnIklanBilling(userId)
@@ -19,16 +17,16 @@ const CardOwnerIklan : React.FC<ICardOwnerIklan> = ({ userId, onClick }) => {
     socket.on('sendInfoOnline', async (res) => {
         console.log('called sendInfoOnline ', res.message)
         const response = await apiGetOwnIklanBilling(userId)
-        setDataBillingOnline(response)
+        setDataBilling(response)
     })
 console.log(dataBilling)
     return (
         <div className='border border-gray-200 p-3 rounded-lg flex flex-row flex-nowrap items-center'>
             <div className='flex-grow flex-shrink'>
-                <h5>{dataBillingOnline ? dataBillingOnline.username : dataBilling.username}</h5>
+                <h5>{dataBilling?.username}</h5>
                 <div className='flex flex-row flex-nowrap items-center mt-1'>
-                    <span className={`${dataBillingOnline?.isOnline ? 'bg-green-500' : 'bg-red-500'} rounded-full h-2 w-2 block mr-1`}></span>
-                    <span>{dataBillingOnline?.isOnline ? 'online' : 'offline'}</span>
+                    <span className={`${dataBilling?.isOnline ? 'bg-green-500' : 'bg-red-500'} rounded-full h-2 w-2 block mr-1`}></span>
+                    <span>{dataBilling?.isOnline ? 'online' : 'offline'}</span>
                 </div>
             </div>
 
